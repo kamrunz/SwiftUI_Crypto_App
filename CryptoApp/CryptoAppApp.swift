6d26cf2f06e67514ddb12c903e1e9c219844da2c
kamrunz <kamraan.namazov@gmail.com>
2022-12-27 12:18:49 +0400

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Kamran Namazov on 27.12.22.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
